016b7194cbd8822434c2ebbe651dccdb48e384f9
r1khil <rikhil.sharma@outlook.com>
2023-04-18 09:30:01 -0400

    up and down by exid

--- main.py
+++ main.py
@@ -6,17 +6,29 @@ import os
 wn = turtle.Screen()
 
 
-lifes = 3
+lifes = 4
 
-points = 0
+points = 1
 
 turtle.register_shape('canada.gif')
 
-turtle.register_shape('usa.gif')
+turtle.register_shape('brazil.gif')
+
+turtle.register_shape('china.gif')
+
+turtle.register_shape('germany.gif')
+
+turtle.register_shape('ireland.gif')
+
+turtle.register_shape('italy.gif')
+
+turtle.register_shape('japan.gif')
+
+turtle.register_shape('sweden.gif')
 
 turtle.register_shape('mexico.gif')
 
-flag = ["canada.gif", 'usa.gif', 'mexico.gif']
+flag = ["canada.gif", 'brazil.gif', 'mexico.gif', 'china.gif', 'german.gif', 'ireland.gif', 'italy.gif', 'japan.gif', 'sweden.gif',]
 
 
 def run():
@@ -31,31 +43,29 @@ def run():
   global answer
   answer = input("What flag is this? ")
 
-print("A flag will pop up on the screen. Enter the country name of the flag. Add a .gif to the end of your answer and use all lowercase. For example, japan should be written as japan.gif. The United States should be written as usa.gif ")
+while True:
 
-run()
+  print("A flag will pop up on the screen. Enter the country name of the flag. Add a .gif to the end of your answer and use all lowercase. For example, japan should be written as japan.gif. The United States should be written as usa.gif ")
 
-while lifes > 0:
-  if answer == flag_chosen:
-    print("slay")
-    points = points + 1
-    print(points)
-    print(lifes)
-    turtle.clearscreen()
-    run()
-  else:
-    lifes = lifes -1
-    print(points)
-    print(lifes)
-    turtle.clearscreen()
-    run()
+  run()
 
-if lifes == 0:
-  print("Sorry, you lost.")  
-  play_again = input("Would you like to play again? (Y/N) ")
-  if play_again == "Y":
-    os.execl(sys.executable, sys.executable, *sys.argv)
-  if play_again == "N":
-    exit()
+  while lifes > 0:
+    if answer == flag_chosen:
+      print("slay")
+      points = points + 1
+      print("Points:", str(points))
+      print("Lifes:", str(lifes))
+      turtle.clearscreen()
+      run()
+    else:
+      lifes = lifes - 1
+      print("Points:", str(points))
+      print("Lifes:", str(lifes))
+      turtle.clearscreen()
+      run()
+    if points == 9:
+      print("You win!")
+      exit()
 
-wn.mainloop()
+
+  wn.mainloop()
